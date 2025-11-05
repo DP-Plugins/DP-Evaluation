8564ba88ce3c7a14cf5d06f07b0eb200e62e96bb
darksoldier1404 <darksoldier1029@gmail.com>
2025-11-05 18:31:38 +0900

    5.3.0 Update

--- src/main/java/com/darksoldier1404/dpev/events/DPEVEvent.java
+++ src/main/java/com/darksoldier1404/dpev/events/DPEVEvent.java
@@ -2,16 +2,16 @@ package com.darksoldier1404.dpev.events;
 
 import com.darksoldier1404.dpev.functions.DPEVFunction;
 import com.darksoldier1404.dppc.api.inventory.DInventory;
+import com.darksoldier1404.dppc.events.dinventory.DInventoryClickEvent;
+import com.darksoldier1404.dppc.events.dinventory.DInventoryCloseEvent;
 import com.darksoldier1404.dppc.utils.NBT;
 import com.darksoldier1404.dppc.utils.Tuple;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 import static com.darksoldier1404.dpev.Evaluation.currentEdit;
 import static com.darksoldier1404.dpev.Evaluation.plugin;
@@ -19,84 +19,86 @@ import static com.darksoldier1404.dpev.Evaluation.plugin;
 public class DPEVEvent implements Listener {
 
     @EventHandler
-    public void onInventoryClose(InventoryCloseEvent e) {
+    public void onInventoryClose(DInventoryCloseEvent e) {
         Player p = (Player) e.getPlayer();
-        if (e.getInventory().getHolder() == null) return;
-        if (e.getInventory().getHolder() instanceof DInventory) {
-            DInventory inv = (DInventory) e.getInventory().getHolder();
-            if (inv.isValidHandler(plugin)) {
-                if (inv.isValidChannel(1)) { // item edit save
-                    DPEVFunction.saveItems(inv);
-                    p.sendMessage("§a아이템 설정이 저장되었습니다.");
-                    return;
+        DInventory inv = e.getDInventory();
+        if (inv.isValidHandler(plugin)) {
+            if (inv.isValidChannel(1)) { // item edit save
+                DPEVFunction.saveItems(inv);
+                p.sendMessage("§a아이템 설정이 저장되었습니다.");
+                return;
+            }
+            if (inv.isValidChannel(101)) {
+                inv.applyChanges();
+                ItemStack item = inv.getItem(13);
+                if (item != null && !item.getType().isAir()) {
+                    p.getInventory().addItem(item.clone());
                 }
-                if (inv.isValidChannel(101)) {
-                    inv.applyChanges();
-                    ItemStack item = inv.getItem(13);
-                    if (item != null && !item.getType().isAir()) {
-                        p.getInventory().addItem(item.clone());
-                    }
-                    return;
-                }
-                if (inv.isValidChannel(102)) { // sell inventory close
-                    inv.applyChanges();
-                    DPEVFunction.returnItems(p, inv);
-                    return;
-                }
+                return;
+            }
+            if (inv.isValidChannel(102)) { // sell inventory close
+                inv.applyChanges();
+                DPEVFunction.returnItems(p, inv);
             }
         }
     }
 
     @EventHandler
-    public void onInventoryClick(InventoryClickEvent e) {
-        if (e.getClickedInventory() == null || e.getClickedInventory().getHolder() == null) {
-            return;
-        }
-        if (e.getClickedInventory().getHolder() instanceof DInventory) {
-            DInventory inv = (DInventory) e.getClickedInventory().getHolder();
-            Player p = (Player) e.getWhoClicked();
-            if (inv.isValidHandler(plugin)) {
+    public void onInventoryClick(DInventoryClickEvent e) {
+        if (e.getClickedInventory() instanceof PlayerInventory && e.getInventory() instanceof DInventory) {
+            DInventory inv = e.getDInventory();
+            if (inv.isValidHandler(plugin) && inv.isValidChannel(101)) {
+                e.setCancelled(true);
+                if (inv.getItem(13) != null) {
+                    return;
+                }
                 ItemStack item = e.getCurrentItem();
                 if (item == null || item.getType().isAir()) {
                     return;
                 }
-                if (inv.isValidChannel(101)) {
-                    if (NBT.hasTagKey(item, "dpevs_confirm")) {
-                        e.setCancelled(true);
-                        inv.applyChanges();
-                        DPEVFunction.evaluateItem(p, inv.getItem(13));
-                        return;
-                    }
-                }
-                if (inv.isValidChannel(102)) { // sell
-                    if (NBT.hasTagKey(item, "dpevs_sell")) {
-                        e.setCancelled(true);
-                        inv.applyChanges();
-                        DPEVFunction.sellItems(p, inv);
-                        return;
-                    }
-                }
-                if (NBT.hasTagKey(item, "dppc_prevpage")) {
+                ItemStack clone = item.clone();
+                item.setAmount(item.getAmount() - 1);
+                e.setCurrentItem(item);
+                clone.setAmount(1);
+                inv.setItem(13, clone);
+                inv.applyChanges();
+                return;
+            }
+        }
+        DInventory inv = e.getDInventory();
+        Player p = (Player) e.getWhoClicked();
+        if (inv.isValidHandler(plugin)) {
+            ItemStack item = e.getCurrentItem();
+            if (item == null || item.getType().isAir()) {
+                return;
+            }
+            if (inv.isValidChannel(101)) {
+                if (e.getSlot() == 13) {
+                    e.setCancelled(true);
+                    p.getInventory().addItem(item.clone());
+                    inv.setItem(13, null);
                     inv.applyChanges();
-                    inv.prevPage();
-                    e.setCancelled(true);
                     return;
                 }
-                if (NBT.hasTagKey(item, "dppc_nextpage")) {
+                if (NBT.hasTagKey(item, "dpevs_confirm")) {
+                    e.setCancelled(true);
+                    DPEVFunction.evaluateItem(p, inv.getItem(13));
                     inv.applyChanges();
-                    inv.nextPage();
-                    e.setCancelled(true);
                     return;
                 }
-                if (NBT.hasTagKey(item, "dppc_clickcancel") || NBT.hasTagKey(item, "dpvs_barrier")) {
+            }
+            if (inv.isValidChannel(102)) { // sell
+                if (NBT.hasTagKey(item, "dpevs_sell")) {
                     e.setCancelled(true);
+                    DPEVFunction.sellItems(p, inv);
+                    inv.applyChanges();
                     return;
                 }
-                if (inv.isValidChannel(2)) { // price edit with chat
-                    p.closeInventory();
-                    plugin.currentEdit.put(p.getUniqueId(), Tuple.of(e.getSlot(), inv));
-                    p.sendMessage("채팅으로 가격을 설정 해주세요. : MIN-MAX");
-                }
+            }
+            if (inv.isValidChannel(2)) { // price edit with chat
+                p.closeInventory();
+                plugin.currentEdit.put(p.getUniqueId(), Tuple.of(e.getSlot(), inv));
+                p.sendMessage("채팅으로 가격을 설정 해주세요. : MIN-MAX");
             }
         }
     }
